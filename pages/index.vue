5ce0b8ea94565f98c031a42b1adbc54f5859bccc
ojaq <abangojaq@gmail.com>
2024-10-28 23:08:58 +0700

    more adjustments

--- pages/index.vue
+++ pages/index.vue
@@ -4,3 +4,14 @@
         <nuxt-link to="/register">Register</nuxt-link>
     </v-container>
 </template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+import { onMounted } from 'vue';
+
+const router = useRouter();
+
+onMounted(() => {
+    router.replace('/register');
+});
+</script>
